b3990d62f7dc733762b11febb428060f47fd009c
Eric Uldall <ericuldall@gmail.com>
2018-03-07 23:36:15 +0100

    Issues/#1 scaling controller prettify (#3)
    * first pass on cleanup for issue #1
    
    * refactored some code into smaller services

---
+++ pkg/services/scaling/step/parser.go
@@ -0,0 +1,17 @@
+package step
+
+import scalingv1alpha1 "k8s.restdev.com/operators/pkg/apis/scaling/v1alpha1"
+
+func Parse (step scalingv1alpha1.ScheduledScalerStep) (min, max *int32) {
+	if step.Mode == "range" {
+		min = step.MinReplicas
+		max = step.MaxReplicas
+	}
+
+	if step.Mode == "fixed" {
+		min = step.Replicas
+		max = step.Replicas
+	}
+
+	return
+}
