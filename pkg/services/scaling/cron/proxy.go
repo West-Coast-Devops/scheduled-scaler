b41e5e13a2ec12a12ff0bd7973bd04d7dbaba15f
Sheridan C Rawlins <scr@verizonmedia.com>
2020-11-12 23:33:05 -0800

    Add more comments.

--- pkg/services/scaling/cron/proxy.go
+++ pkg/services/scaling/cron/proxy.go
@@ -7,6 +7,7 @@ import (
 	"time"
 )
 
+// CronProxy wraps the cron object for testing purposes, as this interface can be mocked.
 type CronProxy interface {
 	Create(timeZone string) *cron.Cron
 	Push(c *cron.Cron, time string, call func())
@@ -14,24 +15,29 @@ type CronProxy interface {
 	Stop(c *cron.Cron)
 }
 
+// CronImpl passes methods through to cron methods.
 type CronImpl struct {
 }
 
+// Create creates a cron object for the given timeZone.
 func (ci *CronImpl) Create(timeZone string) *cron.Cron {
 	l, _ := time.LoadLocation(timeZone)
 
 	return cron.NewWithLocation(l)
 }
 
+// Push pushes the time spec onto the cron, c, with call callback.
 func (ci *CronImpl) Push(c *cron.Cron, time string, call func()) {
 	s, _ := cron.Parse(time)
 	c.Schedule(s, cron.FuncJob(call))
 }
 
+// Start starts the cron object, c.
 func (ci *CronImpl) Start(c *cron.Cron) {
 	c.Start()
 }
 
+// Stop stops the cron object, c.
 func (ci *CronImpl) Stop(c *cron.Cron) {
 	c.Stop()
 }
